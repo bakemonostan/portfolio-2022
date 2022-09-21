b918b24be4481a32247f930b3a48ddbbf8967879
bakemonostan <ehizomokhomion@gmail.com>
2022-09-21 04:33:12 -0700

    updated project images

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,6 +2,7 @@ import url from './projects-images/url-shortener.jpg';
 import dojo from './projects-images/dojo.png';
 import filter from './projects-images/static-filter.jpg';
 import gym from './projects-images/gym.png';
+import taskify from './projects-images/taskify.png';
 import manage from './projects-images/manage.jpg';
 
-export { url, dojo, filter, gym, manage };
+export { url, dojo, filter, gym, manage, taskify };
